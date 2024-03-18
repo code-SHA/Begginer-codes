60f398a1214591c5839f858694ed6f97d92baf55
code-SHA <141864984+code-SHA@users.noreply.github.com>
2024-03-18 22:12:02 +0530

    Delete sorting_a_list.py

--- sorting_a_list.py
+++
@@ -1,9 +0,0 @@
-num=int(input("How many values you have to gave me: "))
-ass=[]
-for i in range(1,num+1):
-    a=int(input("Enter a Number: "))
-    ass.append(a) 
-ass.sort()
-print("Assdending order",ass)
-ass.reverse()
-print("Decending Order",ass)
