c36fe98e055438b5dc2f97aaa247af50fdb310fc
code-SHA <141864984+code-SHA@users.noreply.github.com>
2024-03-18 21:55:32 +0530

    Delete Identify_vowels.py

--- Identify_vowels.py
+++
@@ -1,7 +0,0 @@
-print("IDENTIFYING VOWELS")
-alph=str(input("Enter a Word: "))
-char=alph.lower()
-if (char=="a" or char=="e" or char=="i" or char=="o" or char=="u"):
-    print("Given is VOWEL..!!!")
-else:
-    print("Given is NOT a VOWEL..!!!")
