548cff40012afba2017081768fbc0af278bf0398
code-SHA <141864984+code-SHA@users.noreply.github.com>
2024-03-18 22:15:03 +0530

    Delete sum_of_natural_numbers.py

--- sum_of_natural_numbers.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter a Number: "))
-if n<0:
-    print("Enter a valid number..!")
-else:
-    temp=0
-    while n>0:
-        temp=temp+n
-        n=n-1
-print("The Sum of Natural Number is: ",temp)
