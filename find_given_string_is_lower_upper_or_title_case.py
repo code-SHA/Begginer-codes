8a5f12fe29a353d7e01ba22cd5e94b7d6cc0f435
code-SHA <141864984+code-SHA@users.noreply.github.com>
2024-03-18 22:02:20 +0530

    Delete find_given_string_is_lower_upper_or_title_case.py

--- find_given_string_is_lower_upper_or_title_case.py
+++
@@ -1,7 +0,0 @@
-line=str(input("Enter a String: "))
-if line.islower():
-    print("Your String is LowerCase")
-elif line.istitle():
-    print("Your String is TitleCase")
-else:
-    print("Yout String is UpperCase")
