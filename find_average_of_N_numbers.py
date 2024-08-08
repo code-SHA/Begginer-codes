e26bcf1b7ba951c35ab4ee3a7e8b56c8c7dd3dd8
Dheepan Sathya <141864984+code-SHA@users.noreply.github.com>
2024-08-08 03:31:28 -0700

    Delete find_average_of_N_numbers.py

--- find_average_of_N_numbers.py
+++
@@ -1,7 +0,0 @@
-N=int(input("Enter a N number to find Average: "))
-a=[]
-for i in range(0,N):
-    val=int(input("Enter a Values: "))
-    a.append(val)
-avg=sum(a)/N
-print("Given input's AVg is: ",avg)
