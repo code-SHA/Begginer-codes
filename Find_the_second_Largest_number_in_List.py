35adc6e1b7a3e17963ea8ddab62350d6ad22620a
Dheepan Sathya <141864984+code-SHA@users.noreply.github.com>
2024-08-08 03:26:15 -0700

    Delete Find_the_second_Largest_number_in_List.py

--- Find_the_second_Largest_number_in_List.py
+++
@@ -1,7 +0,0 @@
-list1=[]
-num=int(input("Enter a Range You Gave me: "))
-for i in range(0,num):
-    elements=int(input("Enter the Element: "))
-    list1.append(elements)
-list1.sort()
-print("Second Largest Number is",list1[-2])
