7d50d2f8e27bc9418f34ba0d2db296aee4ad0407
Dheepan Sathya <141864984+code-SHA@users.noreply.github.com>
2024-08-08 03:29:46 -0700

    Delete delete_punctuation_in_sentence.py

--- delete_punctuation_in_sentence.py
+++
@@ -1,7 +0,0 @@
-puntuation='''|()[]{}:;,.!@#$%^&*_+=~`\/<>'''
-s=input("Enter a Sentence: ")
-no_puntuation=""
-for i in s:
-    if i not in puntuation:
-        no_puntuation=no_puntuation+i
-print(no_puntuation)
