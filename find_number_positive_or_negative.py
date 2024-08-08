94973307dc800da3b49f182e280eae0d44f72e57
Dheepan Sathya <141864984+code-SHA@users.noreply.github.com>
2024-08-08 03:32:59 -0700

    Delete find_number_positive_or_negative.py

--- find_number_positive_or_negative.py
+++
@@ -1,6 +0,0 @@
-print("To find Positive or Negative")
-num=int(input("Enter a Number: "))
-if num < 0:
-    print("Given Number",num,"is Negative..!!!")
-else:
-    print("Given Number",num,"is Positive..!!!")
